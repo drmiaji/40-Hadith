3dc7bff8dcf7b92e1c07dd5e26a375dd88092207
Dr Abdul Baten Miaji <bmiaji@gmail.com>
2025-05-21 18:28:47 +0200

    dd

--- app/src/main/java/com/drmiaji/webviewtemplate/ui/dua/DuaListActivity.kt
+++ app/src/main/java/com/drmiaji/webviewtemplate/ui/dua/DuaListActivity.kt
@@ -1,21 +1,32 @@
 package com.drmiaji.webviewtemplate.ui.dua
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.lifecycle.lifecycleScope
 import com.drmiaji.webviewtemplate.data.database.DuaDatabase
+import com.drmiaji.webviewtemplate.data.model.DuaEntity
 import kotlinx.coroutines.launch
 
 class DuaListActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // ... your UI setup and dua list loading ...
 
-        val db = DuaDatabase.getInstance(this)
-        val duaDao = db.duaDao()
-
-        lifecycleScope.launch {
-            val allDuas = duaDao.getAllDua()
-            // TODO: Display the list
+        // Example: to open details for a selected dua
+        fun showDuaDetail(duaId: Int) {
+            lifecycleScope.launch {
+                val dao = DuaDatabase.getInstance(applicationContext).duaDao()
+                val dua: DuaEntity? = dao.getDuaById(duaId)
+                dua?.let {
+                    val intent = Intent(this@DuaListActivity, com.drmiaji.webviewtemplate.WebViewActivity::class.java)
+                    intent.putExtra("dua_title", it.title)
+                    intent.putExtra("dua_content", it.content)
+                    startActivity(intent)
+                }
+            }
         }
+
+        // Call showDuaDetail(someId) when user clicks a dua item
     }
 }
