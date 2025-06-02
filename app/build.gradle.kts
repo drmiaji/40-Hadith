374d7bb216a3c3e6c02bc82553f26cd1e3d184be
Dr Abdul Baten Miaji <bmiaji@gmail.com>
2025-06-02 19:04:41 +0200

    icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.drmiaji.fortyahadith"
         minSdk = 24
         targetSdk = 35
-        versionCode = 11
-        versionName = "2.0"
+        versionCode = 12
+        versionName = "2.1.0"
     }
 
     buildTypes {
